611849e054ca485c0ccdd82d5600d4766f69393e
Furkan Sarı <furkansa@hotmail.com.tr>
2025-03-28 18:31:16 +0300

    style: add basic styles for resume display

--- src/components/resume/ResumeEducationInfo.jsx
+++ src/components/resume/ResumeEducationInfo.jsx
@@ -2,20 +2,29 @@ import React from "react";
 
 const ResumeEducationInfo = () => {
   const educationData = JSON.parse(sessionStorage.getItem("edu-info")) || [];
+  const checkString = (text, searchTerm) =>
+    text?.toLowerCase().includes(searchTerm.toLowerCase());
   return (
     <>
       <h4>Education Information</h4>
       {educationData.map((edu, index) => (
-        <div key={index} className="education-entry">
-          <div className="program">
-            <span className="school-name">{edu.schoolName}</span>
-            <span className="department">{edu.department}</span>
-          </div>
+        <div key={index} className="entry">
           <div className="dates">
             <span className="education-start-date">
               {edu.educationStartDate}
             </span>
-            <span className="education-end-date">{edu.educationEndDate}</span>
+            <span>—</span>
+            <span className="education-end-date">
+              {checkString(edu.educationEndDate, "-") ? (
+                <span className="present">Present</span>
+              ) : (
+                <span>{edu.educationEndDate}</span>
+              )}
+            </span>
+          </div>
+          <div className="program">
+            <span className="school-name">{edu.schoolName}</span>
+            <span className="department">{edu.department}</span>
           </div>
         </div>
       ))}
