0d784716de3cfe87815aae0e2630ee928aaee4cc
Furkan Sarı <furkansa@hotmail.com.tr>
2025-06-03 21:21:30 +0300

    style: add basic styling for skills/langs/hobbies

--- src/components/output/ResumeAside.jsx
+++ src/components/output/ResumeAside.jsx
@@ -19,62 +19,67 @@ const ResumeAside = ({ personalInfo, skillInfo, languageInfo, hobbyInfo }) => {
 
   return (
     <aside>
-      <div className="photo">
-        <img
-          src={personalInfo[0].photo || blankPP}
-          alt="Profile-Photo"
-          className="profile-photo"
-        />
+      <div className="essential-information">
+        <div className="photo">
+          <img
+            src={personalInfo[0].photo || blankPP}
+            alt="Profile-Photo"
+            className="profile-photo"
+          />
+        </div>
+        <div className="contact-info">
+          {email && (
+            <a href={`mailto:${email}`}>
+              {" "}
+              <LuMail className="contact-icon" /> {email}
+            </a>
+          )}
+          {phone && (
+            <a href={`tel:${phone}`}>
+              <LuPhone className="contact-icon" /> {phone}
+            </a>
+          )}
+          {portfolio && (
+            <a
+              href={`https://www.${portfolio}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="contact-icon" /> {portfolio}
+            </a>
+          )}
+          {shortAddress && (
+            <p>
+              <FaMapMarkerAlt className="contact-icon" /> {shortAddress}
+            </p>
+          )}
+        </div>
+
+        {/* other sections will appear here in the future */}
+        <div className="skills-and-languages">
+          {skillInfo?.length > 0 && (
+            <>
+              <h4>Skills</h4>
+              <ul>
+                {skillInfo.map((skill, i) => (
+                  <li key={`skill-${i}`}>{skill}</li>
+                ))}
+              </ul>
+            </>
+          )}
+          {languageInfo?.length > 0 && (
+            <>
+              <h4>Languages</h4>
+              <ul>
+                {languageInfo.map((lang, i) => (
+                  <li key={`lang${i}`}>{lang}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
       </div>
-      <div className="contact-info">
-        {email && (
-          <a href={`mailto:${email}`}>
-            {" "}
-            <LuMail className="contact-icon" /> {email}
-          </a>
-        )}
-        {phone && (
-          <a href={`tel:${phone}`}>
-            <LuPhone className="contact-icon" /> {phone}
-          </a>
-        )}
-        {portfolio && (
-          <a
-            href={`https://www.${portfolio}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub className="contact-icon" /> {portfolio}
-          </a>
-        )}
-        {shortAddress && (
-          <p>
-            <FaMapMarkerAlt className="contact-icon" /> {shortAddress}
-          </p>
-        )}
-      </div>
-      {/* other sections will appear here in the future */}
-      <div className="skills-and-languages">
-        {skillInfo?.length > 0 && (
-          <>
-            <h4>Skills</h4>
-            <ul>
-              {skillInfo.map((skill, i) => (
-                <li key={`skill-${i}`}>{skill}</li>
-              ))}
-            </ul>
-          </>
-        )}
-        {languageInfo?.length > 0 && (
-          <>
-            <h4>Languages</h4>
-            <ul>
-              {languageInfo.map((lang, i) => (
-                <li key={`lang${i}`}>{lang}</li>
-              ))}
-            </ul>
-          </>
-        )}
+      <div className="hobbies">
         {hobbyInfo?.length > 0 && (
           <>
             <h4>Hobbies</h4>
